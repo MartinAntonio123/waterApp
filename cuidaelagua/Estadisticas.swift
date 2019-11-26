66af58ea794ad33f012ea57a3185c8d69f88ef31
Martin Vivanco <martinvivanco@MacBook-Pro-de-POSGRADO.local>
2019-11-26 09:26:16 -0600

    first commit

---
+++ cuidaelagua/Estadisticas.swift
@@ -0,0 +1,30 @@
+//
+//  Estadisticas.swift
+//  cuidaelagua
+//
+//  Created by Martin Vivanco on 11/6/19.
+//  Copyright © 2019 Martin Vivanco. All rights reserved.
+//
+
+import UIKit
+
+class Estadisticas: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
